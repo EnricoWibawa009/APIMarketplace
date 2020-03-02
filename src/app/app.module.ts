6ff3c1bf8ac60f8a488a2d524b035254aef09f41
Aga Putra <7316006@student.unpar.ac.id>
2020-03-02 09:41:09 +0700

    tambah header_weatherAPI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BillingComponent } from './billing/billing.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderWeatherComponent } from './header_weatherAPI/header_weatherAPI.component';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { BillingComponent } from './billing/billing.component';
     BillingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HeaderWeatherComponent]
 })
 export class AppModule { }
---
+++ src/app/header_weatherAPI/header_weatherAPI.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header_weather',
+  templateUrl: './header_weather.component.html',
+  styleUrls: ['./header_weather.component.css']
+})
+export class HeaderWeatherComponent {
+}
