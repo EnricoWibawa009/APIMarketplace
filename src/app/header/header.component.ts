230ee58016e690ad8bc7e2402238286d0b23e817
giogioanggasta <giogioanggasta@gmail.com>
2020-03-05 15:28:22 +0700

    header footer selesai

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'header',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  constructor(){}
+
+  ngOnInit(){}
   
 }
+  
+
+
