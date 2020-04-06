5bd776c02444c050ef951f47a23637aaf10b9229
Reynaldiirfan <reynaldiirfannn@gmail.com>
2020-04-06 19:34:12 +0700

    finishing design dashboard

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { LoginModule } from './app/login/login.module';
-<<<<<<< HEAD
-import { NavbarModule } from './app/navbar/navbar.module';
-import { SidebarModule} from './app/sidebarAPI/sidebarAPI.module';
-=======
 import { AppModule } from './app/app.module';
->>>>>>> 22e62e9d8607a02f38336e7741c88f090c3b16ba
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -15,10 +11,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic()
-<<<<<<< HEAD
-  .bootstrapModule(LoginModule)
-=======
-
   .bootstrapModule(AppModule)
->>>>>>> 22e62e9d8607a02f38336e7741c88f090c3b16ba
   .catch(err => console.error(err));
