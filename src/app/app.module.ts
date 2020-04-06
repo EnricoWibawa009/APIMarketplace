281813f6fbf9dd6ccb9494b228bff3fa7e5817f8
agame77 <agame7.ap@gmail.com>
2020-04-06 08:23:26 +0700

    testing routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,6 +10,17 @@ import { MatCardModule } from '@angular/material/card';
 import { HeaderWeatherComponent } from './header_weatherAPI/header_weatherAPI.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component:LoginComponent},
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+]
 
 
 
@@ -26,13 +37,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
-  
+export class AppModule {
+
 }
 
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import {FooterComponent} from '../footer/footer.component';
+import {HeaderComponent} from '../header/header.component';
 
 @NgModule({
     imports: [],
-    declarations: [FooterComponent],
+    declarations: [FooterComponent,
+    HeaderComponent],
     bootstrap: [ ]
   })
   export class DashBoardModule {
