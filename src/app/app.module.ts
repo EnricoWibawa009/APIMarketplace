5e3ca98ddd00e50869fc563ddb45a072fb928d51
agame77 <agame7.ap@gmail.com>
2020-04-13 08:56:47 +0700

    update review

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
-import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderWeatherComponent } from './header_weatherAPI/header_weatherAPI.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
-// import { ReviewComponent } from './review/review.component';
+import { ReviewComponent } from './review/review.component';
 // import { NavbarComponent } from './navbar/navbar.component';
+// import { SidebarComponent } from './sidebarAPI.component';
+// import { BillingComponent } from './sidebarAPI.component';
+// import { SubscribeComponent } from './sidebarAPI.component';
+
 
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component:LoginComponent},
-  { path: 'marketplace', component:MarketplaceComponent},
-  // { path: 'review', component:ReviewComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'marketplace', component: MarketplaceComponent },
+  { path: 'review', component:ReviewComponent},
   // { path: 'navbar', component:NavbarComponent},
   {
     path: '',
@@ -41,9 +59,23 @@ const routes: Routes = [
     LoginComponent
   ],
   imports: [
+    CommonModule,
+    ReactiveFormsModule,
     BrowserModule,
     BrowserAnimationsModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatListModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
     MatButtonModule,
+    MatDialogModule,
+    MatIconModule,
     MatCardModule,
     RouterModule.forRoot(routes)
   ],
--- src/app/review/review.module.ts
+++ src/app/review/review.module.ts
@@ -1,23 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   imports: [
-    MatSelectModule,
-    MatListModule
   ],
   declarations: [
     FooterComponent,
     HeaderComponent],
   bootstrap: []
 })
-<<<<<<< HEAD
+
 export class ReviewModule {
-=======
-export class NavbarModule {
->>>>>>> 5cb104ab91da4117a4e75b7d203206c56fc60d84
   hide = true;
 }
