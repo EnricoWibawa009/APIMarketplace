9e047b6879a523a9ef44d29ae442c310e53405b9
agame77 <agame7.ap@gmail.com>
2020-04-20 11:38:32 +0700

    update review

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { getParseTreeNode } from 'typescript';
 
 export interface Section {
+  rating: number;
+  color: string;
   name: string;
-  updated: Date;
+  date: string;
+  comment: string;
 }
 
 @Component({
@@ -16,13 +20,53 @@ export class ReviewComponent {
 
   notes: Section[] = [
     {
-      name: 'Vacation Itinerary',
-      updated: new Date('2/20/16'),
+      rating: 5,
+      color: 'green',
+      name: 'Robert Reed',
+      date: 'December 27, 2019',
+      comment: 'Lorem ipsum dolor sit amet conseqtetur adipsicing elit tempor dolor magna',
     },
     {
-      name: 'Kitchen Remodel',
-      updated: new Date('1/18/16'),
-    }
+      rating: 4,
+      color: 'green',
+      name: 'Susan Bingham',
+      date: 'December 27, 2019',
+      comment: 'Lorem ipsum dolor sit amet conseqtetur adipsicing elit tempor dolor magna',
+    },
+    {
+      rating: 3,
+      color: 'green',
+      name: 'Jackblack Longnamous',
+      date: 'December 27, 2019',
+      comment: 'Lorem ipsum dolor sit amet conseqtetur adipsicing elit tempor dolor magna',
+    },
+    {
+      rating: 5,
+      color: 'green',
+      name: 'David Roberts',
+      date: 'December 27, 2019',
+      comment: 'Lorem ipsum dolor sit amet conseqtetur adipsicing elit tempor dolor magna',
+    },
+    // {
+    //   rating: 2,
+    //   color: 'green',
+    //   name: 'David Roberts',
+    //   date: 'December 27, 2019',
+    //   comment: 'Bad',
+    // },
+    // {
+    //   rating: 3,
+    //   // if(rating: 5) {
+    //   //   color: 'green';
+    //   // },
+    //   // else {
+    //   //   color: 'red';
+    //   // },
+    //   color: 'green',
+    //   name: 'Debby Moran',
+    //   date: 'December 27, 2019',
+    //   comment: 'Lorem ipsum dolor sit amet conseqtetur adipsicing elit tempor dolor magna',
+    // }
   ];
 }
 
