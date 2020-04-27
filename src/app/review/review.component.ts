27a39fbd3b6cd925f7b9c80326af4de4c968db04
agame77 <agame7.ap@gmail.com>
2020-04-27 08:32:45 +0700

    review hampir beres

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -16,7 +16,11 @@ export interface Section {
 })
 export class ReviewComponent {
   hide = true;
-
+  status1: boolean = false;
+  status2: boolean = false;
+  status3: boolean = false;
+  status4: boolean = false;
+  status5: boolean = false;
 
   notes: Section[] = [
     {
@@ -68,5 +72,42 @@ export class ReviewComponent {
     //   comment: 'Lorem ipsum dolor sit amet conseqtetur adipsicing elit tempor dolor magna',
     // }
   ];
+
+clickEvent1(){
+  this.status1 = !this.status1; 
+  this.status2 = false;
+  this.status3 = false;
+  this.status4 = false;
+  this.status5 = false;       
+} 
+clickEvent2(){
+  this.status2 = !this.status2;
+  this.status1 = false;
+  this.status3 = false;
+  this.status4 = false;
+  this.status5 = false;       
+}
+clickEvent3(){
+  this.status3 = !this.status3;   
+  this.status2 = false;
+  this.status1 = false;
+  this.status4 = false;
+  this.status5 = false;      
+}
+clickEvent4(){
+  this.status4 = !this.status4;
+  this.status2 = false;
+  this.status1 = false;
+  this.status3 = false;
+  this.status5 = false;      
+}
+clickEvent5(){
+  this.status5 = !this.status5;
+  this.status2 = false;
+  this.status1 = false;
+  this.status4 = false;
+  this.status3 = false;        
+}
+
 }
 
