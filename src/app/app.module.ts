03b5200a4d2128298bf9afb107a09b75f4057fe5
Aga Putra <7316006@student.unpar.ac.id>
2020-04-27 09:35:24 +0700

    routing link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MarketplaceComponent } from './marketplace/marketplace.component';
 import { ReviewComponent } from './review/review.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { APIDetailComponent } from './APIDetail/APIDetail.component';
 // import { SidebarComponent } from './sidebarAPI.component';
 // import { BillingComponent } from './sidebarAPI.component';
 // import { SubscribeComponent } from './sidebarAPI.component';
@@ -38,7 +39,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'marketplace', component: MarketplaceComponent },
   { path: 'review', component:ReviewComponent},
-  { path: 'navbar',  component:NavbarComponent},
+  { path: 'apiDetail', component:APIDetailComponent},
+  //{ path: 'navbar',  component:NavbarComponent},
   {
     path: '',
     redirectTo: 'login',
@@ -57,7 +59,8 @@ const routes: Routes = [
     MarketplaceComponent,
     ReviewComponent,
     LoginComponent,
-    NavbarComponent
+    NavbarComponent,
+    APIDetailComponent
   ],
   imports: [
     CommonModule,
